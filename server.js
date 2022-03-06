a3f039fa7259779fee8c86d14a941d4cff5f8ae3
shylag1073 <shylag1073@gmail.com>
2022-03-05 19:59:07 -0600

    final working product, still trying to get bonus working

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ let path = require("path")
 let app=express();
 let PORT = 80
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+});
 
 app.get("/notes", (req, res) => {
     res.sendFile (path.join(__dirname,"./public/notes.html")) ;
@@ -19,19 +21,29 @@ app.get("*" , (req, res) => {
     res.sendFile (path.join(__dirname,"./public/index.html")) ;
 })
 
-app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./db/db.json"));
-    res.json(path.join(__dirname, "./db/db.json"));
-});
+
 
 app.post ("/api/notes", (req ,res) => {
     const newNote = req.body;
-    const notes = JSON.parse ( fs.readFileSync("./db/db.json"));
+    let notes = JSON.parse ( fs.readFileSync("./db/db.json"));
     newNote.id = uniqueID();
     notes.push(newNote);
     fs.writeFileSync("./db/db.json", JSON.stringify(notes));
     res.json(notes);
 })
+
+// app.delete("/api/notes/:id", (req ,res) => {
+
+//     let newnotes = require('./db/db.json')
+// for (let i = 0; i < newnotes.length; i++) {
+//     if(newnotes[i].id == req.params.id){
+//         newnotes.splice(i, 1)
+//     }
+//     fs.writeFileSync('./db/db.json', JSON.stringify(newnotes))
+//     res.json(newnotes)
+    
+// }
+// })
 
 app.listen(process.env.PORT || PORT, () => {
     console.log('Server Running in http://localhost')
