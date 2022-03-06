7eb94c3c57d0226692cde7ff4e231b3121c73d33
shylag1073 <shylag1073@gmail.com>
2022-03-05 19:34:27 -0600

    adding

--- server.js
+++ server.js
@@ -3,34 +3,38 @@ let uniqueID = require("uniqID");
 let fs =require("fs");
 let path = require("path")
 let app=express();
+let PORT = 80
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+
 app.get("/notes", (req, res) => {
-    res.sendFile (path.join(__dirname,"./Develop/public/notes.html")) ;
+    res.sendFile (path.join(__dirname,"./public/notes.html")) ;
 })
 
 app.get("*" , (req, res) => {
-    res.sendFile (path.join(__dirname,"./Develop/public/index.html")) ;
+    res.sendFile (path.join(__dirname,"./public/index.html")) ;
 })
 
 app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./Develop/db/db.json"));
-    res.json(path.join(__dirname, "./Develop/db/db.json"));
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+    res.json(path.join(__dirname, "./db/db.json"));
 });
 
 app.post ("/api/notes", (req ,res) => {
     const newNote = req.body;
-    const notes = JSON.parse ( fs.readFileSync("./Develop/db/db.json"));
+    const notes = JSON.parse ( fs.readFileSync("./db/db.json"));
     newNote.id = uniqueID();
     notes.push(newNote);
-    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(notes));
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes));
     res.json(notes);
 })
 
-app.listen(process.env.PORT || 3009, () => {
-    console.log('Server Running in Port 3001')
+app.listen(process.env.PORT || PORT, () => {
+    console.log('Server Running in http://localhost')
   });
 
 
